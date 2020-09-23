9cefddeac54b63f3ba48349c065342a55a35650b
MattRM2 <matthieu.barbie@gmail.com>
2020-09-23 15:58:35 +0200

    WIP

--- EX_CLI_001/my-first-vue-cli-app/src/App.vue
+++ EX_CLI_001/my-first-vue-cli-app/src/App.vue
@@ -1,20 +1,41 @@
-<script>
-import NavLink from "./components/HomeLink.vue";
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/Test" v-if="test === true">Test</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
 
+<script>
 export default {
-  name: "App",
-  components: {
-    NavLink,
+  data() {
+    return { test: true };
   },
 };
 </script>
 
-<template>
-  <div id="app">
-    <nav>
-      <NavLink url="/" text="Accueil" />
-      <NavLink url="/about" text="About" />
-      <NavLink url="/contact" text="Contact" />
-    </nav>
-  </div>
-</template>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ EX_CLI_001/my-first-vue-cli-app/src/views/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>My test route with Vue.js</h1>
+  </div>
+</template>
