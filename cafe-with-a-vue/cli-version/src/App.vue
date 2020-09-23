380e204c6018d6699c15ca870920400a72c81ac9
MattRM2 <matthieu.barbie@gmail.com>
2020-09-23 14:48:17 +0200

    Update App.vue

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -61,40 +61,42 @@
 export default {
   name: "app",
   data() {
-    address: "18 avenue du Beurre, Paris, France",
-    email: "hello@cafewithavue.bakery",
-    phone: "01 88 88 88 88",
-    restaurantName: "La belle vue",
-    shoppingCart: 0,
-    simpleMenu: [
-      {
-        name: "Croissant",
-        image: {
-          source: "./images/croissant.jpg",
-          alt: "Un croissant",
+    return {
+      address: "18 avenue du Beurre, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "01 88 88 88 88",
+      restaurantName: "La belle vue",
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: "Croissant",
+          image: {
+            source: "./images/croissant.jpg",
+            alt: "Un croissant",
+          },
+          inStock: true,
+          quantity: 1,
         },
-        inStock: true,
-        quantity: 1,
-      },
-      {
-        name: "Baguette de pain",
-        image: {
-          source: "./images/french-baguette.jpeg",
-          alt: "Quatre baguettes de pain",
+        {
+          name: "Baguette de pain",
+          image: {
+            source: "./images/french-baguette.jpeg",
+            alt: "Quatre baguettes de pain",
+          },
+          inStock: true,
+          quantity: 1,
         },
-        inStock: true,
-        quantity: 1,
-      },
-      {
-        name: "Éclair",
-        image: {
-          source: "./images/eclair.jpg",
-          alt: "Éclair au chocolat",
+        {
+          name: "Éclair",
+          image: {
+            source: "./images/eclair.jpg",
+            alt: "Éclair au chocolat",
+          },
+          inStock: false,
+          quantity: 1,
         },
-        inStock: false,
-        quantity: 1,
-      },
-    ],
+      ],
+    };
   },
   computed: {
     copyright() {
